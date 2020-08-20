b86e84b33ab1fd0d2a4d22e81bf1974fa71b1fd3
yotoz <thdrudwls7@gmail.com>
2020-08-20 20:36:45 +0900

    Updates

---
+++ precache-manifest.9a421212d6c4a0988172f1f48034774d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23f2fc6adc554d5970f4788c7a0e7e6",
+    "url": "/kakako-login-test/index.html"
+  },
+  {
+    "revision": "aecb2df5d21e444c6c3d",
+    "url": "/kakako-login-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0926c57b20f1c9a3fbf9",
+    "url": "/kakako-login-test/static/js/2.f1dba77b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kakako-login-test/static/js/2.f1dba77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aecb2df5d21e444c6c3d",
+    "url": "/kakako-login-test/static/js/main.bcdb403b.chunk.js"
+  },
+  {
+    "revision": "82ee477fb3238de00d82",
+    "url": "/kakako-login-test/static/js/runtime-main.de1ad13c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kakako-login-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
